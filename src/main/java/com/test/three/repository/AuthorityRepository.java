eaa20fa9f74be9e6f2c34080db026877ae28e189
Rashmi-Goli <rashmihgoli@gmail.com>
2021-07-21 18:36:54 -0700

    Initial version of three generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/test/three/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.test.three.repository;
+
+import com.test.three.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/com/test/three/ArchTest.java
@@ -0,0 +1,29 @@
+package com.test.three;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.test.three");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.test.three.service..")
+            .or()
+            .resideInAnyPackage("com.test.three.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.test.three.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
